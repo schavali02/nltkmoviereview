3cc645de67c0ca5dcd467971363fa4847c478afd
schavali02 <67966905+schavali02@users.noreply.github.com>
2020-08-09 11:01:43 -0700

    Add files via upload

---
+++ textanalysis.py
@@ -0,0 +1,9 @@
+'''
+Created on Aug 4, 2020
+
+@author: shashankchavali
+'''
+import textanalysismodule as s
+
+print(s.sentiment("This movie was great."))
+print(s.sentiment("This was a horrible movie."))
